134600a58c9a1c6fb34099b26b21df1d85620811
Roy Lopez <roylopezdev@gmail.com>
2024-04-04 07:59:37 -0300

    Refactor authMiddleware to use express-oauth2-jwt-bearer

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,18 +1,9 @@
-import "dotenv/config";
+import { auth } from "express-oauth2-jwt-bearer";
+const issuerBaseURL = process.env.AUTH0_ISSUER_BASE_URL;
+const audience = process.env.AUTH0_AUDIENCE || "http://localhost:8000/api";
 
-import { GetVerificationKey, expressjwt as jwt } from "express-jwt";
-import jwksRsa from "jwks-rsa";
-const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
-const audience = process.env.AUTH0_AUDIENCE || "http://localhost:5000/";
-
-export const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`,
-  }) as GetVerificationKey,
-  audience: audience,
-  issuer: `${issuerBaseUrl}/`,
-  algorithms: ["RS256"],
+export const checkJwt = auth({
+  audience,
+  issuerBaseURL,
+  tokenSigningAlg: "RS256",
 });
